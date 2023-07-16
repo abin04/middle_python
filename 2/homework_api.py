5f18c1f0d8f13c8f59de6fe15394aecc00c665d3
abin04 <abin04@yandex.ru>
2023-07-16 12:27:05 +0300

    add DB

--- 2/homework_api.py
+++ 2/homework_api.py
@@ -3,6 +3,11 @@ import requests
 from aiohttp import ClientSession, TCPConnector
 import logging
 from bs4 import BeautifulSoup
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import String, Integer
 
 async def get_vacancy(id, session):
     """
@@ -19,18 +24,20 @@ async def main(ids):
     функция принимает список id вакансий и возвращает список кортежей с параметрами вакансий 
     """
     connector = TCPConnector(limit=5)
-    async with ClientSession('https://api.hh.ru/', connector=connector) as session:
+    async with ClientSession('https://api.hh.ru/', connector=connector, trust_env=True) as session:
         tasks = []
         for id in ids:
             tasks.append(asyncio.create_task(get_vacancy(id, session)))
         results = await asyncio.gather(*tasks)
     for result in results:
-        skills_name =''
-        for skills in result['key_skills']:
-            skills_name += skills['name'] +", "
-        data_prep.append((result['name'], result['employer']['name'],
-                          BeautifulSoup(result['description'],'lxml').get_text(),
-                          skills_name))
+        if result['key_skills']:
+            skills_name =''
+            for skills in result['key_skills']:
+                skills_name += skills['name'] +", "
+            data_prep.append((result['name'], result['employer']['name'],
+                            BeautifulSoup(result['description'],'lxml').get_text(),
+                            skills_name))
+    session.close
     return data_prep
 logging.basicConfig(filename='my_app.log', 
                     encoding='utf-8', 
@@ -44,14 +51,41 @@ url_params = {
     # "page": "1",
 }
 data_prep = []
-vacancies_ids = []
-for i in range(5):
-    # while len(data_prep)<100:
-    page = {"page":str(i)}
-    params = {**url_params, **page}
-    result = requests.get(url, params=params)
-    vacancies = result.json().get('items')
-    for vacancy in vacancies:
-        vacancies_ids.append(vacancy['id'])
-asyncio.run(main(vacancies_ids ))
-print(data_prep)
+for i in range(10):
+    if len(data_prep)<100:
+        vacancies_ids = []
+        page = {"page":str(i)}
+        params = {**url_params, **page}
+        result = requests.get(url, params=params)
+        vacancies = result.json().get('items')
+        logging.debug(f"Получена {i} страница поиска вакансий")
+        for vacancy in vacancies:
+            vacancies_ids.append(vacancy['id'])
+        asyncio.run(main(vacancies_ids ))
+        logging.debug(f"Получено {len(data_prep)} вакансий")
+# print(data_prep)
+Base = declarative_base()
+class Vacancy_prep(Base):
+    __tablename__ = 'vacancies'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    company_name = Column('company_name', String)
+    position = Column('position', String)
+    job_description = Column('job_description', String)
+    key_skills = Column('key_skills', String)
+    def __init__(self, company_name, position, job_description, key_skills):
+        self.company_name = company_name
+        self.position = position
+        self.job_description = job_description
+        self.key_skills = key_skills
+engine = create_engine("sqlite:///hw2.db", echo=True)
+Base.metadata.create_all(bind=engine)
+Sesion = sessionmaker(bind=engine)
+session = Sesion()
+for record in data_prep:
+    new_record = Vacancy_prep(company_name=record['company_name'],
+                              position=record['position'],
+                              job_description=record['job_descripton'],
+                              key_skills=record['key_skills'])
+    session.add(new_record)
+session.commit()
+session.close()
