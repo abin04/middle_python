03f2a721de2de3af3fedf1b927a84e422f9db1fa
abin04 <57984391+abin04@users.noreply.github.com>
2023-08-12 10:16:11 +0300

    name_corp where one "

--- 3/dags/sql/update_records_telecom.sql
+++ 3/dags/sql/update_records_telecom.sql
@@ -1,1 +1,1 @@
-UPDATE telecom_companies SET name_corp = substring(name_corp from Strpos(name_corp,'"')+1 for Length(name_corp)-Strpos(name_corp,'"')-1) WHERE Strpos(name_corp,'"')>0; 
+UPDATE telecom_companies SET name_corp = substring(name_corp from Strpos(name_corp,'"')+1 for Length(name_corp)-Strpos(name_corp,'"')-1) WHERE Length(name_corp)>Strpos(name_corp,'"'); 
