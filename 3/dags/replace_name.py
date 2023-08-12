cbf1d6db6a8a5fc0e3edf964f6547d86d20ce4bb
abin04 <57984391+abin04@users.noreply.github.com>
2023-08-12 22:49:17 +0300

    Update replace_name.py

--- 3/dags/replace_name.py
+++ 3/dags/replace_name.py
@@ -1,15 +1,18 @@
 import psycopg2
 from config import config
 conn = None
-data_prep = [(84756985,''),(83279672,''),(83279672,''),(83279672,''),(83279672,''),(83279672,''),(83279672,''),(83279672,''),(83279672,''),
-             (83279672,''),(83279672,''),(83279672,''),(83279672,''),(83279672,''),(83279672,''),(83279672,''),(83279672,''),(83279672,''),
-             (83279672,''),(83279672,'')]
 try:
     params = config()
     conn = psycopg2.connect(**params)
     cur = conn.cursor()
-    for id, names in data_prep:
-        insert_several_rows_parameters = f'UPDATE vacancies SET company_name={names} WHERE id={id};'
+    cur.execute('select name_corp from telecom_companies limit 20;')
+    names=cur.fetchall()
+    cur.execute('select id from vacancies limit 20;')
+    ids=cur.fetchall()
+    for i in range(20):
+        name=names[i]
+        id=ids[i]
+        insert_several_rows_parameters = f"UPDATE vacancies SET company_name = '{name[0]}' WHERE id = {id[0]};"
         cur.execute(insert_several_rows_parameters)
     conn.commit()
     cur.close()     
@@ -18,3 +21,4 @@ except (Exception, psycopg2.DatabaseError) as error:
 finally:
     if conn is not None:
         conn.close() 
+
